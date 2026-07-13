401ed75639db154bc7f5ae303c53e47e7fe130b2
kimhun <>
2026-07-13 22:34:05 +0700

    feat: func

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const INFLATION_RATE = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var expectedReturnRate float64
 	var years float64
@@ -20,8 +21,7 @@ func main() {
 	fmt.Print("Enter the number of years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
-	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	// formattedFutureValue := fmt.Sprintf("Future Value of Investment: $%.2f\n", futureValue)
 	// formattedFutureRealValue := fmt.Sprintf("Future Real Value of Investment (Adjusted for Inflation): $%.2f\n", futureRealValue)
@@ -33,4 +33,11 @@ func main() {
 
 Future Real Value of Investment (Adjusted for Inflation): $%.2f
 `, futureValue, futureRealValue)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	rf := fv / math.Pow((1+INFLATION_RATE/100), years)
+
+	return fv, rf
 }
