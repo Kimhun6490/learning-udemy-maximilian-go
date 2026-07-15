ccb204b9990e0529456dc2d52963a3f991dd7904
kimhun <>
2026-07-16 00:34:37 +0700

    feat: sol

--- app.go
+++ app.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// 1)
+	hobbies := [3]string{"Reading", "Traveling", "Cooking"}
+	fmt.Println("Hobbies:", hobbies)
+
+	// 2)
+	fmt.Println("Hobbies:", hobbies[0])
+	fmt.Println("Hobbies:", hobbies[1:3])
+
+	// 3)
+	mainHobbies := hobbies[:2]
+	fmt.Println("Main Hobbies:", mainHobbies)
 }
