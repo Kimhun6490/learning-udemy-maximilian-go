6c232cfda4c809111bdd2dfbe993314413aab411
kimhun <>
2026-07-16 00:05:53 +0700

    feat: impl generic

--- app.go
+++ app.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	resultInt := add(5, 10)
+	fmt.Println(resultInt)
+
+	resultFloat := add(5.5, 10.5)
+	fmt.Println(resultFloat)
+
+	resultString := add("Hello, ", "World!")
+	fmt.Println(resultString)
+}
+
+func add[T int | float64 | string](a, b T) T {
+	return a + b
 }
