e05e1c3ba65c0bf787610cd6f6fe55dfa52b6d2b
kimhun <>
2026-07-19 11:19:17 +0700

    feat: go routines

--- app.go
+++ app.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func greet(name string, done chan bool) {
+	fmt.Printf("Hello, %s!\n", name)
+	done <- true // Signal that the goroutine is done
+}
+
+func slowGreet(name string, done chan bool) {
+	time.Sleep(2 * time.Second)
+	fmt.Printf("Hello, %s!\n", name)
+	done <- true // Signal that the goroutine is done
+	close(done)  // Close the channel to signal that no more values will be sent
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// dones := make([]chan bool, 4)
+	// for i := range dones {
+	// 	dones[i] = make(chan bool)
+	// }
+
+	// go greet("Step 1", dones[0])
+	// go greet("Step 2", dones[1])
+	// go slowGreet("Step 3", dones[2])
+	// go greet("Step 4", dones[3])
+
+	// for i := range dones {
+	// 	<-dones[i] // Wait for each goroutine to finish
+	// }
+
+	done := make(chan bool)
+	go greet("Step 1", done)
+	go greet("Step 2", done)
+	go slowGreet("Step 3", done)
+	go greet("Step 4", done)
+
+	for range done {
+
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/goroutine_greeting
+
+go 1.26.2
